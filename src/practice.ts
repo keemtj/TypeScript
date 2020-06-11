727ed6fcb90f3ee94ec62750700f4bdfd6a8d5fc
keemtj <keemgreat@gmail.com>
2020-06-11 15:38:02 +0900

    typescript variable

---
+++ src/practice.ts
@@ -0,0 +1,15 @@
+let count: number = 0;
+count += 1;
+
+const message: string = "hello world";
+const done: boolean = false;
+
+const numbers: number[] = [1, 2, 3];
+
+const messages: string[] = ["hello", "world"];
+
+let mightBeUndefined: string | undefined = undefined; // 'dfasdf'
+let nullableNumber: number | null = null; // 10
+
+let color: "red" | "orange" | "yellow" = "red";
+color = "yellow";
