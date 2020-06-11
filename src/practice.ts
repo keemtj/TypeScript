5f5323940e1222cbd947e9813fd072b6a73847f3
keemtj <keemgreat@gmail.com>
2020-06-11 16:57:44 +0900

    typescript class, queue

--- src/practice.ts
+++ src/practice.ts
@@ -1,33 +1,28 @@
-// Generics
-// parameter type을 any로 넣어준 것처럼 어떤 값이든 넣어줄 수 있는데
-// any를 사용하게 되면 type이 any로 보여지지만
-// 제너릭스를 사용하면 param에 따라 string, number등 보여준다
-// 즉 어떤값이든 넣고 싶고, any는 싫고, type을 보여주고 싶을때 제너릭스
-function merge<T1, T2>(a: T1, b: T2) {
-  return {
-    ...a,
-    ...b,
-  };
+// Queue(FIFO)
+
+class Queue<T> {
+  list: T[] = [];
+
+  get length() {
+    return this.list.length;
+  }
+
+  enqueue(item: T) {
+    this.list.push(item);
+  }
+
+  dequeue() {
+    return this.list.shift();
+  }
 }
 
-const merged = merge({ foo: 1 }, { bar: 2, foobar: 3 });
+const queue = new Queue<number>();
+queue.enqueue(0);
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
 
-function wrap<T>(param: T) {
-  return {
-    param,
-  };
+while (queue.length > 0) {
+  console.log(queue.dequeue());
 }
-
-const wrappded = wrap("10");
-wrappded.param;
-
-// interface Items<T> {
-type Items<T, V> = {
-  list: T[];
-  value: V;
-};
-
-const items: Items<string, number> = {
-  list: ["a", "b", "c"],
-  value: 1,
-};
